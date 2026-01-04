fbdd6227454e942438179ba1154faff9ec671fa6
feddiriko <github@feddiriko.no>
2026-01-04 20:12:32 +0100

    Update logging message in async_setup_entry

--- custom_components/Norwegian_Policelog/__init__.py
+++ custom_components/Norwegian_Policelog/__init__.py
@@ -1,5 +1,5 @@
 import logging
-from homeassistant import config_entries
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
@@ -7,7 +7,7 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: config_entries.ConfigEntry):
-    _LOGGER.info("Police log integration setup via UI")
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    _LOGGER.info("Norwegian Police Log integration loaded")
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
