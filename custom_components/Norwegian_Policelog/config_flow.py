64c6a00daf38dcb98814afedc993ccf8bbf032d0
feddiriko <github@feddiriko.no>
2026-01-04 20:12:18 +0100

    Refactor config flow for Norwegian Police Log

--- custom_components/Norwegian_Policelog/config_flow.py
+++ custom_components/Norwegian_Policelog/config_flow.py
@@ -1,30 +1,27 @@
 import logging
 from homeassistant import config_entries
-import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 
 _LOGGER = logging.getLogger(__name__)
 
-class PoliceLogConfigFlow(config_entries.ConfigFlow, domain="norwegian_policelog"):
+DOMAIN = "norwegian_policelog"
+
+class PoliceLogConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            district = user_input["district"]
-            municipality = user_input["municipality"]
-
             return self.async_create_entry(
-                title=f"Police Log {district}",
-                data={
-                    "district": district,
-                    "municipality": municipality
-                }
+                title=f"Police Log {user_input['district']}",
+                data=user_input,
             )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("district", default="SørVest"): cv.string,
-                vol.Optional("municipality", default=""): cv.string,
-            })
+            data_schema=vol.Schema(
+                {
+                    vol.Required("district", default="Oslo"): str,
+                    vol.Optional("municipality", default=""): str,
+                }
+            ),
         )
